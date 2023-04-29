a361342c7e3a65a4ebed8d12d91a6cd81dcd3352
Jackson <yinelona@gmail.com>
2023-04-29 02:20:50 -0700

    worldlight index-complete update v_1.0

--- js/main.js
+++ js/main.js
@@ -131,86 +131,97 @@
 $(document).ready(function(){
 
   // Range Slider
-  if ( $( '[data-slider]' ).length ) {
-      $( '[data-slider]' ).slider({});
+    if ( $( '[data-slider]' ).length ) {
+        $( '[data-slider]' ).slider({});
 
-      $( '#price_slider' ).on( 'slide', function( evt ) {
-          $( '#price_slider_value span' ).html( '$' + evt.value[0] + ' - ' + '$' + evt.value[1] );
-      });
-  }
+        $( '#price_slider' ).on( 'slide', function( evt ) {
+            $( '#price_slider_value span' ).html( '$' + evt.value[0] + ' - ' + '$' + evt.value[1] );
+        });
+    }
 
 
   // QTY
   if ( $('.quantity').length ) {
-      $('.quantity').each(function() {
-          var $this = $(this);
-          var $qty = $this.find('.qty');
-          var $plusBtn = $this.find('#plus-btn');
-          var $minusBtn = $this.find('#minus-btn');
+        $('.quantity').each(function() {
+            var $this = $(this);
+            var $qty = $this.find('.qty');
+            var $plusBtn = $this.find('#plus-btn');
+            var $minusBtn = $this.find('#minus-btn');
 
-          $plusBtn.click(function(evt) {
-              evt.preventDefault();
-              $qty.val(parseInt($qty.val()) + 1 );
-          });
-          $minusBtn.click(function(evt) {
-              evt.preventDefault();
-              $qty.val(parseInt($qty.val()) - 1 );
-              if ($qty.val() == 0) {
-                  $qty.val(1);
-              }
-          });
-      });
-  }
+            $plusBtn.click(function(evt) {
+                evt.preventDefault();
+                $qty.val(parseInt($qty.val()) + 1 );
+            });
+            $minusBtn.click(function(evt) {
+                evt.preventDefault();
+                $qty.val(parseInt($qty.val()) - 1 );
+                if ($qty.val() == 0) {
+                    $qty.val(1);
+                }
+            });
+        });
+    }
 
-  $('.banner').slick({
-      autoplay: false,
-      autoplaySpeed: 2000,
-      speed: 800,
-      dots: false,
-      arrows: true,
-      infinite: true,
-      fade: true,
-      cssEase: 'linear',
-      appendArrows: $('.indicator'),
-      nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-left"></i></button>',
-      prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-right"></i></button>',
-  });
+    $('.banner').slick({
+        autoplay: false,
+        autoplaySpeed: 2000,
+        speed: 800,
+        dots: false,
+        arrows: true,
+        infinite: true,
+        cssEase: 'linear',
+        appendArrows: $('#indicator'),
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-left"></i></button>',
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-right"></i></button>',
+    });
 
-  //Logo slider
-  $('.logos').slick({
-      infinite: true,
-      dots: false,
-      arrows: false,
-      speed: 800,
-      rtl: false,
-      cssEase: 'linear',
-      slidesToShow: 6,
-      slidesToScroll: 6,
-      responsive: [
-          {
-              breakpoint: 1200,
-              settings: {
-                  infinite: true,
-                  slidesToShow: 4,
-                  slidesToScroll: 4,
-              },
-          },
-          {
-              breakpoint: 768,
-              settings: {
-                  infinite: true,
-                  slidesToShow: 3,
-                  slidesToScroll: 3,
-              },
-          },
-          {
-              breakpoint: 480,
-              settings: {
-                  infinite: true,
-                  slidesToShow: 2,
-                  slidesToScroll: 2,
-              },
-          },
-      ],
-  });
+    $('.new-arrival-slide').slick({
+        autoplay: false,
+        autoplaySpeed: 2000,
+        speed: 800,
+        dots: true,
+        arrows: true,
+        infinite: true,
+        cssEase: 'linear',
+        appendArrows: $('#new-arrival-indicator'),
+        appendDots: $('#new-arrival-indicator'),
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-left"></i></button>',
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-right"></i></button>',
+        responsive: [
+            {
+                breakpoint: 1280,
+                settings: 'unslick',
+            }
+        ],
+    });
+
+    //Logo slider
+    $('.logos').slick({
+        infinite: true,
+        dots: false,
+        arrows: false,
+        speed: 800,
+        rtl: false,
+        cssEase: 'linear',
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
 });
